7b0285410c3a07cba312423cab4fddbecb3dc8e3
Zach Dillon <zach@shapeways.com>
2017-05-09 11:52:54 +0000

    Zach dev

--- playground.py
+++ playground.py
@@ -1,9 +1,23 @@
 import json
-import ast
 import requests
-from pprint import pprint
+from flask import Flask, redirect, url_for, session, request
+from flask_oauthlib.client import OAuth, OAuthException
+from secret import client_s
 
-with open('data.txt', 'r') as data_file:
-    data = data_file.read()
-value = data[1:-1]
-print value
+app = Flask(__name__, template_folder="./public", static_folder="./src")
+
+
+@app.route('/')
+def test_access():
+    with open('data.txt') as f:
+        data = f.read()
+    access = data[1:-1]
+    headers = {"Authorization": "bearer " + access}
+    response = requests.get("https://api.shapeways.com/api/v1", headers=headers)
+    response_json = json.loads(response.text)
+    result = response_json['result']
+    return str(result)
+
+
+if __name__ == '__main__':
+    app.run()
