5ba6af3d609e057ca06f8a9f1ef77910319926cc
zachdillon <zach@shapeways.com>
2017-06-26 16:11:05 -0400

    Added Error status bar, yet to be tested.

--- src/components/MoveAlert.js
+++ src/components/MoveAlert.js
@@ -2,24 +2,18 @@ import React, { Component } from 'react';
 import { Progress } from 'reactstrap';
 
 class MoveAlert extends Component {
-  // timer() {
-  //   let counter = 0;
-  //   let i = setInterval(function(){
-  //
-  //     counter++;
-  //     if(counter === 10) {
-  //       clearInterval(i);
-  //     }
-  //   }, 200);
-  // }
-
   handleFeedback() {
     let result = this.props.result;
-    // let timeOutput = this.timer()
     if (result === "success") {
       return(
         <div>
           <Progress color="success" value="100">Move Successful! You're Awesome!</Progress>
+        </div>
+      )
+    } else if (result === "failure") {
+      return(
+        <div>
+          <Progress color="danger" value="100">Whoops! There was a problem with the request.</Progress>
         </div>
       )
     } else {
