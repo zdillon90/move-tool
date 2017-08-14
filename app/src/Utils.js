f2b5458e97819eaa030f20a77bca7f146f43623f
zachdillon <zach@shapeways.com>
2017-08-14 11:57:19 -0400

    Got refresh token from storage
    Still need to fix the call after the fix to to it after the new access token it retreaved

--- app/src/Utils.js
+++ app/src/Utils.js
@@ -5,12 +5,28 @@ import queryString from 'query-string';
 import config from './inshape_config.json';
 
 
-const tokenPromise = new Promise((resolve, reject) => {
+const accessTokenPromise = new Promise((resolve, reject) => {
   storage.has('token', (error, hasKey) => {
     if (hasKey) {
       storage.get('token', (err, data) => {
         if (err) throw err;
-        resolve(data);
+        console.log(`Promise Access Token: ${data.access_token}`);
+        resolve(data.access_token);
+      });
+    } else {
+      reject(Error(`Storage Error: ${error}`));
+    }
+  });
+});
+
+// TODO Make a new Promise for the Refresh Token
+const refreshTokenPromise = new Promise((resolve, reject) => {
+  storage.has('token', (error, hasKey) => {
+    if (hasKey) {
+      storage.get('token', (err, data) => {
+        if (err) throw err;
+        console.log(`Promise Refresh Token: ${data.refresh_token}`);
+        resolve(data.refresh_token);
       });
     } else {
       reject(Error(`Storage Error: ${error}`));
@@ -24,7 +40,7 @@ axios.interceptors.response.use(undefined, (err) => {
   let res = err.response;
   // TODO Add a condition to chek to see if the reason was bearer experation
   if (res.status === 400 && res.config && !res.config.__isRetryRequest) {
-    return getRefreshToken(err.config)
+    return getRefreshToken()
     .then((success) => {
       if (success !== undefined) {
         console.log(`Success: ${success}`);
@@ -44,50 +60,58 @@ axios.interceptors.response.use(undefined, (err) => {
   throw err;
 });
 
-function getRefreshToken(data) {
+function getRefreshToken() {
   return new Promise((resolve, reject) => {
-    console.log(data.headers.refresh);
-    const body = {
-      grant_type: 'refresh_token',
-      client_id: config.clientId,
-      refresh_token: data.headers.refresh
-    };
-    const refreshReq = {
-      method: 'post',
-      url: 'https://api.shapeways.com/oauth2/token',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      data: queryString.stringify(body)
-    };
-    console.log('Trying to get new token');
-    refreshReq.headers.Authorization = 'Basic ' + new Buffer(config.clientId + ':' + config.clientSecret).toString('base64');
-    // TODO that refresh token is only given out once, I need to grab it form storage
-    axios(refreshReq)
-      .then((response) => {
-        console.log(response);
-        if (response.status === 200) {
-          resolve(response.data);
-        } else {
-          reject(Error(response));
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    refreshTokenPromise.then((refreshToken) => refreshToken)
+    .then((token) => {
+      console.log(`refresh token: ${token}`);
+      const body = {
+        grant_type: 'refresh_token',
+        client_id: config.clientId,
+        refresh_token: token
+      };
+      const refreshReq = {
+        method: 'post',
+        url: 'https://api.shapeways.com/oauth2/token',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: queryString.stringify(body)
+      };
+      return refreshReq;
+    })
+    .then((request) => {
+      console.log('Trying to get new token');
+      request.headers.Authorization = 'Basic ' + new Buffer(config.clientId + ':' + config.clientSecret).toString('base64');
+      // TODO that refresh token is only given out once, I need to grab it form storage
+      axios(request)
+        .then((response) => {
+          console.log(response);
+          if (response.status === 200) {
+            resolve(response.data);
+          } else {
+            reject(Error(response));
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
   });
 }
 
 export function InshapeAPI(requestMethod, endpoint, body) {
   return new Promise((resolve, reject) => {
-    tokenPromise.then((data) => data)
-    .then((token) => {
+    accessTokenPromise.then((data) => data)
+    .then((accessToken) => {
       const req = {
         method: requestMethod,
         url: endpoint,
         headers: {
-          authorization: `bearer ${token.access_token}`,
-          refresh: token.refresh_token
+          authorization: `bearer ${accessToken}`
         }
       };
 
@@ -96,7 +120,7 @@ export function InshapeAPI(requestMethod, endpoint, body) {
       }
 
       console.log('Inshape Call');
-      console.log(token);
+      console.log(accessToken);
       axios(req)
         .then((response) => {
           console.log(response);
